c5c2637ca0586438535ea7d0108a8d90ae78feda
ageorge95 <arteni.george.daniel@gmail.com>
2022-05-30 15:25:33 +0300

    added some example code for super classes chained inheritance

---
+++ chained_multi_inheritance/main.py
@@ -0,0 +1,30 @@
+# The MRO works from the bottom to the top
+
+class First():
+    # it is a good practice to let people know
+    # that this variable will be used in the inheritor
+    final_arg: int
+
+    def __init__(self):
+        super(First, self).__init__()
+        print("first", self.final_arg)
+
+class Second():
+    # it is a good practice to let people know
+    # that this variable will come from the inheritor
+    final_arg: int
+
+    def __init__(self):
+        print("second", self.final_arg)
+        super(Second, self).__init__()
+
+
+class Third(First,
+            Second):
+
+    def __init__(self):
+        self.final_arg = 0
+        print('third')
+        super(Third, self).__init__()
+
+obj = Third()
